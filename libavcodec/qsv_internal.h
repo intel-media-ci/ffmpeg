53181caec0aecf497615c689ef30238c362f8f9a
Zhong Li <zhong.li@intel.com>
2019-01-18 20:08:23 +0800

    lavc/qsv: add function ff_qsv_map_picstruct()
    Signed-off-by: Zhong Li <zhong.li@intel.com>

--- libavcodec/qsv_internal.h
+++ libavcodec/qsv_internal.h
@@ -94,6 +94,8 @@ int ff_qsv_profile_to_mfx(enum AVCodecID codec_id, int profile);
 int ff_qsv_map_pixfmt(enum AVPixelFormat format, uint32_t *fourcc);
 enum AVPictureType ff_qsv_map_pictype(int mfx_pic_type);
 
+enum AVFieldOrder ff_qsv_map_picstruct(int mfx_pic_struct);
+
 int ff_qsv_init_internal_session(AVCodecContext *avctx, mfxSession *session,
                                  const char *load_plugins);
 
