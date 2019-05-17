b58f784614c0f031425cbf1cc33a7e4ffefbacb4
Fu Linjie <linjie.fu@intel.com>
2019-05-17 14:53:10 -0400

    lavu/pixdesc: fix the step for y210/y410
    The step stands for:
    Number of elements between 2 horizontally consecutive pixels.
    Elements are bits for bitstream formats, bytes otherwise
    
    For 10 bit format, 2 bytes/pixel should be taken into consideration.
    
    See P010 and NV12:
    NV12: 1, 2, 2
    P010: 2, 4, 4
    
    Signed-off-by: Fu Linjie <linjie.fu@intel.com>

--- libavutil/pixdesc.c
+++ libavutil/pixdesc.c
@@ -211,9 +211,9 @@ static const AVPixFmtDescriptor av_pix_fmt_descriptors[AV_PIX_FMT_NB] = {
         .log2_chroma_w = 1,
         .log2_chroma_h = 0,
         .comp = {
-            { 0, 2, 0, 6, 10, 1, 9, 1 },        /* Y */
-            { 0, 4, 1, 6, 10, 3, 9, 2 },        /* U */
-            { 0, 4, 3, 6, 10, 3, 9, 4 },        /* V */
+            { 0, 4, 0, 6, 10, 1, 9, 1 },        /* Y */
+            { 0, 8, 1, 6, 10, 3, 9, 2 },        /* U */
+            { 0, 8, 3, 6, 10, 3, 9, 4 },        /* V */
         },
         .flags = AV_PIX_FMT_FLAG_ALPHA,
     },
@@ -223,9 +223,9 @@ static const AVPixFmtDescriptor av_pix_fmt_descriptors[AV_PIX_FMT_NB] = {
         .log2_chroma_w = 1,
         .log2_chroma_h = 0,
         .comp = {
-            { 0, 2, 0, 6, 10, 1, 9, 1 },        /* Y */
-            { 0, 4, 1, 6, 10, 3, 9, 2 },        /* U */
-            { 0, 4, 3, 6, 10, 3, 9, 4 },        /* V */
+            { 0, 4, 0, 6, 10, 1, 9, 1 },        /* Y */
+            { 0, 8, 1, 6, 10, 3, 9, 2 },        /* U */
+            { 0, 8, 3, 6, 10, 3, 9, 4 },        /* V */
         },
         .flags = AV_PIX_FMT_FLAG_BE | AV_PIX_FMT_FLAG_ALPHA,
     },
@@ -247,10 +247,10 @@ static const AVPixFmtDescriptor av_pix_fmt_descriptors[AV_PIX_FMT_NB] = {
         .log2_chroma_w = 0,
         .log2_chroma_h = 0,
         .comp = {
-            { 0, 4, 1, 0, 10, 3, 9, 2 },        /* Y */
-            { 0, 4, 0, 0, 10, 3, 9, 1 },        /* U */
-            { 0, 4, 2, 0, 10, 3, 9, 3 },        /* V */
-            { 0, 4, 3, 0,  2, 3, 1, 4 },        /* A */
+            { 0, 8, 1, 0, 10, 3, 9, 2 },        /* Y */
+            { 0, 8, 0, 0, 10, 3, 9, 1 },        /* U */
+            { 0, 8, 2, 0, 10, 3, 9, 3 },        /* V */
+            { 0, 8, 3, 0,  2, 3, 1, 4 },        /* A */
         },
         .flags = AV_PIX_FMT_FLAG_ALPHA,
     },
@@ -260,10 +260,10 @@ static const AVPixFmtDescriptor av_pix_fmt_descriptors[AV_PIX_FMT_NB] = {
         .log2_chroma_w = 0,
         .log2_chroma_h = 0,
         .comp = {
-            { 0, 4, 1, 0, 10, 3, 9, 2 },        /* Y */
-            { 0, 4, 0, 0, 10, 3, 9, 1 },        /* U */
-            { 0, 4, 2, 0, 10, 3, 9, 3 },        /* V */
-            { 0, 4, 3, 0,  2, 3, 1, 4 },        /* A */
+            { 0, 8, 1, 0, 10, 3, 9, 2 },        /* Y */
+            { 0, 8, 0, 0, 10, 3, 9, 1 },        /* U */
+            { 0, 8, 2, 0, 10, 3, 9, 3 },        /* V */
+            { 0, 8, 3, 0,  2, 3, 1, 4 },        /* A */
         },
         .flags = AV_PIX_FMT_FLAG_BE | AV_PIX_FMT_FLAG_ALPHA,
     },
