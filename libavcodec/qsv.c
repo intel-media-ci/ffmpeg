9ae33e6819214864cf6b753da1a50bf98695f502
Linjie Fu <linjie.fu@intel.com>
2019-06-28 12:33:23 -0400

    lavc/qsv: add decode support for HEVC Rext
    Add support for YUV422P/YUV422P10, YUV444P/YUV444P10 in qsv_map_pixfmt.
    Allow qsv to choose correct fourc from format.
    
    Add support for YUYV422/Y210/AYUV/Y410 to map frame to surface and
    allowed hwdownload.
    
    HEVC Rext decode is supported on ICL+ platform.
    
    Cmdline for 444 8 bit decode:
    ffmpeg -hwaccel qsv -hwaccel_device /dev/dri/renderD128 -c:v
    hevc_qsv -load_plugin hevc_hw -i HEVCRext_444_8bit.bin -vf
    hwdownload,format=ayuv -pix_fmt ayuv -vsync passthrough out.yuv
    
    Signed-off-by: Linjie Fu <linjie.fu@intel.com>

--- libavcodec/qsv.c
+++ libavcodec/qsv.c
@@ -225,6 +225,18 @@ int ff_qsv_map_pixfmt(enum AVPixelFormat format, uint32_t *fourcc)
     case AV_PIX_FMT_NV12:
         *fourcc = MFX_FOURCC_NV12;
         return AV_PIX_FMT_NV12;
+    case AV_PIX_FMT_YUV422P:
+        *fourcc = MFX_FOURCC_YUY2;
+        return AV_PIX_FMT_YUYV422;
+    case AV_PIX_FMT_YUV422P10:
+        *fourcc = MFX_FOURCC_Y210;
+        return AV_PIX_FMT_Y210LE;
+    case AV_PIX_FMT_YUV444P:
+        *fourcc = MFX_FOURCC_AYUV;
+        return AV_PIX_FMT_AYUV;
+    case AV_PIX_FMT_YUV444P10:
+        *fourcc = MFX_FOURCC_Y410;
+        return AV_PIX_FMT_Y410LE;
     case AV_PIX_FMT_YUV420P10:
     case AV_PIX_FMT_P010:
         *fourcc = MFX_FOURCC_P010;
