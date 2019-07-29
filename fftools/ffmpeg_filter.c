a8802b923492eb352ace23b9cc9a0e6a65f55442
Linjie Fu <linjie.fu@intel.com>
2019-07-29 20:56:34 +0800

    fftools/ffmpeg_filter: add -autoscale to disable/enable the default scale
    Currently, ffmpeg inserts scale filter by default in the filter graph
    to force the whole decoded stream to scale into the same size with the
    first frame. It's not quite make sense in resolution changing cases if
    user wants the rawvideo without any scale.
    
    Using autoscale/noautoscale to indicate whether auto inserting the scale
    filter in the filter graph:
        -noautoscale or -autoscale 0:
        disable the default auto scale filter inserting.
    
    Update docs.
    
    Signed-off-by: U. Artie Eoff <ullysses.a.eoff@intel.com>
    Signed-off-by: Linjie Fu <linjie.fu@intel.com>

--- fftools/ffmpeg_filter.c
+++ fftools/ffmpeg_filter.c
@@ -469,7 +469,7 @@ static int configure_output_video_filter(FilterGraph *fg, OutputFilter *ofilter,
     if (ret < 0)
         return ret;
 
-    if (ofilter->width || ofilter->height) {
+    if ((ofilter->width || ofilter->height) && fg->autoscale) {
         char args[255];
         AVFilterContext *filter;
         AVDictionaryEntry *e = NULL;
