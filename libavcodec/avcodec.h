a6ca20a490305069b51325e9821d9f86e4e2dac5
Linjie Fu <linjie.fu@intel.com>
2020-06-02 14:18:34 +0800

    lavc/avcodec: Add FF_PROFILE_HEVC_SCC for screen content coding
    Version bump is needed while upstreaming.
    
    Signed-off-by: Linjie Fu <linjie.fu@intel.com>

--- libavcodec/avcodec.h
+++ libavcodec/avcodec.h
@@ -1948,6 +1948,7 @@ typedef struct AVCodecContext {
 #define FF_PROFILE_HEVC_MAIN_10                     2
 #define FF_PROFILE_HEVC_MAIN_STILL_PICTURE          3
 #define FF_PROFILE_HEVC_REXT                        4
+#define FF_PROFILE_HEVC_SCC                         9
 
 #define FF_PROFILE_AV1_MAIN                         0
 #define FF_PROFILE_AV1_HIGH                         1
