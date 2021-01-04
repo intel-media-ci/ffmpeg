f14203c5fbc0c9ddd3721df6b1a4957721eeb754
Wenbinc-Bin <wenbin.chen@intel.com>
2021-01-04 09:52:34 +0800

    libavcodec/qsvenc: add low latency P-pyramid support for qsv
    Add low latency P-pyramid support for qsv, and it relates to a new
    command line parameter "-p_strategy". To enable this flag, user also
    need to set "-bf" to 0. P-strategy has two modes "1-simple" and "2-pyramid".
    The details of the two models refer to
    https://github.com/Intel-Media-SDK/MediaSDK/blob/master/doc/mediasdk-man.md#PRefType
    
    Signed-off-by Wenbin Chen <wenbin.chen@intel.com>

--- libavcodec/qsvenc.c
+++ libavcodec/qsvenc.c
@@ -268,6 +268,14 @@ static void dump_video_param(AVCodecContext *avctx, QSVEncContext *q,
     case MFX_B_REF_PYRAMID: av_log(avctx, AV_LOG_VERBOSE, "pyramid");   break;
     default:                av_log(avctx, AV_LOG_VERBOSE, "auto");      break;
     }
+
+    av_log(avctx, AV_LOG_VERBOSE, "; PRefType: ");
+    switch (co3->PRefType) {
+    case MFX_P_REF_DEFAULT: av_log(avctx, AV_LOG_VERBOSE, "default");   break;
+    case MFX_P_REF_SIMPLE:  av_log(avctx, AV_LOG_VERBOSE, "simple");    break;
+    case MFX_P_REF_PYRAMID: av_log(avctx, AV_LOG_VERBOSE, "pyramid");   break;
+    default:                av_log(avctx, AV_LOG_VERBOSE, "unknown");   break;
+    }
     av_log(avctx, AV_LOG_VERBOSE, "\n");
 #endif
 
@@ -783,6 +791,28 @@ FF_ENABLE_DEPRECATION_WARNINGS
 #if QSV_HAVE_CO3
         q->extco3.Header.BufferId      = MFX_EXTBUFF_CODING_OPTION3;
         q->extco3.Header.BufferSz      = sizeof(q->extco3);
+
+        switch (q->p_strategy) {
+        case 0:
+            q->extco3.PRefType = MFX_P_REF_DEFAULT;
+            break;
+        case 1:
+            q->extco3.PRefType = MFX_P_REF_SIMPLE;
+            break;
+        case 2:
+            q->extco3.PRefType = MFX_P_REF_PYRAMID;
+            break;
+        default:
+            q->extco3.PRefType = MFX_P_REF_DEFAULT;
+            av_log(avctx, AV_LOG_WARNING, "invalid p_strategy, set to default\n");
+            break;
+        }
+        if ((q->extco3.PRefType == MFX_P_REF_SIMPLE ||
+            q->extco3.PRefType == MFX_P_REF_PYRAMID) &&
+            avctx->max_b_frames != 0) {
+            av_log(avctx, AV_LOG_WARNING, "Please set max_b_frames(-bf) to 0 to enable P-pyramid\n");
+        }
+
 #if QSV_HAVE_GPB
         if (avctx->codec_id == AV_CODEC_ID_HEVC)
             q->extco3.GPB              = q->gpb ? MFX_CODINGOPTION_ON : MFX_CODINGOPTION_OFF;
