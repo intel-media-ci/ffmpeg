b3a02e56b3654fda791fe24e1477a1251597523a
Shubhanshu Saxena <shubhanshu.e01@gmail.com>
2021-05-08 17:40:15 +0530

    lavfi/dnn_backend_native_layer_dense.h: Documentation
    Add documentation for Dense Layer
    
    Signed-off-by: Shubhanshu Saxena <shubhanshu.e01@gmail.com>

--- libavfilter/dnn/dnn_backend_native_layer_dense.h
+++ libavfilter/dnn/dnn_backend_native_layer_dense.h
@@ -31,7 +31,33 @@ typedef struct DenseParams{
     float *biases;
 } DenseParams;
 
+/**
+ * @brief Load the Densely-Connnected Layer.
+ *
+ * It assigns the layer parameters to the hyperparameters
+ * like activation, bias, and kernel size after parsing
+ * from the model file context.
+ *
+ * @param layer pointer to the DNN layer instance
+ * @param model_file_context pointer to model file context
+ * @param file_size model file size
+ * @param operands_num number of operands for the layer
+ * @return Size of DNN Layer
+ * @retval 0 if model file context contains invalid hyperparameters.
+ */
 int ff_dnn_load_layer_dense(Layer *layer, AVIOContext *model_file_context, int file_size, int operands_num);
+
+/**
+ * @brief Execute the Densely-Connnected Layer.
+ *
+ * @param operands input operands
+ * @param input_operand_indexes input operand indexes
+ * @param output_operand_index output operand index
+ * @param parameters layer parameters
+ * @param ctx pointer to Native model context
+ * @retval DNN_SUCCESS if the execution succeeds
+ * @retval DNN_ERROR if the execution fails
+ */
 int ff_dnn_execute_layer_dense(DnnOperand *operands, const int32_t *input_operand_indexes,
                                int32_t output_operand_index, const void *parameters, NativeContext *ctx);
 #endif
