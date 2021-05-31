352501c02562540085dcfbf33ff4f4706f61adfe
Chen,Wenbin <wenbin.chen@intel.com>
2021-05-31 13:36:35 +0800

    libavutil/hwcontext_vulkan: Add one_memory flag to make vulkan compatible with vaapi device.
    Vaapi can import external surface, but all the planes of the external
    frames should be in the same drm object. A new flag is introduced and
    vulkan can choose to allocate planes in one memory according this flag.
    This flag will be enabled when the vulkan device is derived from vaapi
    device, so that this change will not affect current vulkan behaviour.
    
    Signed-off-by: Wenbin,Chen <wenbin.chen@intel.com>

--- libavutil/hwcontext_vulkan.c
+++ libavutil/hwcontext_vulkan.c
@@ -211,6 +211,9 @@ typedef struct VulkanDevicePriv {
     /* Settings */
     int use_linear_images;
 
+    /* map all planes to one memory */
+    int use_one_memory;
+
     /* Nvidia */
     int dev_is_nvidia;
 } VulkanDevicePriv;
@@ -1321,6 +1324,11 @@ static int vulkan_device_create_internal(AVHWDeviceContext *ctx,
     if (opt_d)
         p->use_linear_images = strtol(opt_d->value, NULL, 10);
 
+    opt_d = av_dict_get(opts, "one_memory", NULL, 0);
+    if (opt_d)
+        p->use_one_memory = strtol(opt_d->value, NULL, 10);
+
+
     hwctx->enabled_dev_extensions = dev_info.ppEnabledExtensionNames;
     hwctx->nb_enabled_dev_extensions = dev_info.enabledExtensionCount;
 
@@ -1443,8 +1451,10 @@ static int vulkan_device_derive(AVHWDeviceContext *ctx,
             return AVERROR_EXTERNAL;
         }
 
-        if (strstr(vendor, "Intel"))
+        if (strstr(vendor, "Intel")) {
+            av_dict_set_int(&opts, "one_memory", 1, 0);
             dev_select.vendor_id = 0x8086;
+        }
         if (strstr(vendor, "AMD"))
             dev_select.vendor_id = 0x1002;
 
